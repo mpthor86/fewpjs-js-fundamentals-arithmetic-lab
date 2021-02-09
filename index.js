f5a9b6419c9ad3e12b61609ae3efbe4d651065b4
michael <mpthornton86@gmail.com>
2021-02-09 11:01:40 -0700

    commit tree d69123a2e96d9002016ada9fd3d927c855b865d9

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let num3 = 27
+let num4 = 23
+
+const multiply = num1 * num2
+
+const random = Math.round(Math.random() * 10) + 1
+
+const mod = num3 % num4
+
+const max = Math.max(2, 3, 5, 20)
